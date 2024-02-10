1625013793a9ca13ddfb9aef54c1ae2fefbb4696
Jakub Ryška <ryskajakub@seznam.cz>
2024-02-10 12:11:32 +0100

    don't focus on chat in switch if chat was not previously focused tab

--- background.js
+++ background.js
@@ -2,22 +2,49 @@ const checkTabIsTeams = (tab) => {
     return tab.url.startsWith("https://teams.microsoft.com")
 }
 
-let focused = false
-
-const ChatBarId = "app-bar-86fcd49b-61a2-4701-b771-54728cd291fb"
-const ActivityBarId = "app-bar-14d6962d-6eeb-4f48-8890-de55454bb136"
+let lastTeamsFocused = false
+let lastTabActive = "Chat"
 
 function checkFocusedWindow() {
     chrome.windows.getAll({ populate: true }, function (windows) {
-        windows.forEach(focusedWindow => {
-            const tab = focusedWindow.tabs[0]
-            if (focusedWindow.type === "app" && checkTabIsTeams(tab) && focusedWindow.focused !== focused) {
-                focused = focusedWindow.focused
-                const elementId = focusedWindow.focused ? ChatBarId : ActivityBarId
-                const func = (elementId) => { 
-                    document.getElementById(elementId).click() 
+        windows.forEach(async maybeTeamsWindow => {
+            const tab = maybeTeamsWindow.tabs[0]
+            if (maybeTeamsWindow.type === "app" && checkTabIsTeams(tab) && maybeTeamsWindow.focused !== lastTeamsFocused) {
+                lastTeamsFocused = maybeTeamsWindow.focused
+
+                const func = (lastTabActive, focused) => {
+
+                    const getButtonText = (e) => {
+                        return [...e.querySelectorAll(".app-bar-text")][0]?.innerText
+                    }
+
+                    const click = (tabName) => {
+                        const found = [...document.getElementsByClassName("app-bar-button")].find(button => { 
+                            const buttonText = getButtonText(button)
+                            return buttonText === tabName
+                        })
+                        found.click()
+                    }
+
+                    if (focused) {
+                        if (lastTabActive === "Chat") {
+                            click("Chat")
+                        }
+                    } else {
+                        const selected = [...document.getElementsByClassName("app-bar-button app-bar-selected")][0]
+                        if (getButtonText(selected) === "Chat") {
+                            click("Activity")
+                            return "Chat"
+                        }
+                    }
                 }
-                chrome.scripting.executeScript({ target: { tabId: tab.id }, func, args: [elementId] })
+                
+                const returnedValue = await chrome.scripting.executeScript({ target: { tabId: tab.id }, func, 
+                    args: [lastTabActive, maybeTeamsWindow.focused] })
+
+                lastTabActive = returnedValue[0]?.result
+
+
             }
         })
     });
